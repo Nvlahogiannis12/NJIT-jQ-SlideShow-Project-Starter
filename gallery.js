d9c95ea515143995e0b999308aee331445c62067
Nick Vlahogiannis <526Nvlahogiannis@frhsd.com>
2025-11-13 10:27:25 -0500

    Async Fetching works

--- gallery.js
+++ gallery.js
@@ -17,17 +17,29 @@ $(document).ready(() => {
   });
 
   // Select the "Next Photo" button and add a click event to call showNextPhoto
+  $(".nextPhoto").on("click", function () {
+    showNextPhoto();
+  });
   // Select the "Previous Photo" button and add a click event to call showPrevPhoto
-
+  $(".prevPhoto").on("click", function () {
+    showPrevPhoto();
+  });
   // Call fetchJSON() to load the initial set of images
   fetchJSON();
 });
 
 // Function to fetch JSON data and store it in mImages
-function fetchJSON() {
+async function fetchJSON() {
   // Use $.ajax here to request the JSON data from mUrl
+  let list = await fetch("images.json");
+  list = await list.json();
+  list.forEach((item) => {
+    mImages.push(item.imgPath);
+  });
+  console.log(list, mImages);
   // On success, parse the JSON and push each image object into mImages array
   // After JSON is loaded, call swapPhoto() to display the first image
+  swapPhoto();
 }
 
 // Function to swap and display the next photo in the slideshow
